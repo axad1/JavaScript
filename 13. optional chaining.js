2a8e7e94418e2531c4572b2a040ce39d25882b87
AxAd <asadhusn007@gmail.com>
2023-10-18 22:35:49 +0500

    optional chaining in arrays

--- 13. optional chaining.js
+++ 13. optional chaining.js
@@ -1,7 +1,15 @@
 user = {
-    firstname : "asad",
-    // address : {house: 1234}
+  firstname: "asad",
+  // address : {house: 1234}
+};
 
-}
+console.log(user?.address);
 
-console.log(user?.address)
+// --------------------
+
+// Optional chaining with array (Important)
+
+// const arr = [1, 2, 3];
+const arr = null;
+
+console.log(arr?.[0]); // .? is valid with array also
